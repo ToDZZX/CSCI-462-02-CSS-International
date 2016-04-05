03c749006a4e1ef27a2e631cbb79e056d339884c
ToDZZX <todzzx@gmail.com>
2016-04-05 17:55:17 -0400

    Added automatic file encryption on upload to server.
    Added automatic file encryption on upload to server.

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var path = require('path');
 var busboy = require('connect-busboy');
+var encryptor = require('file-encryptor');
+var fs = require('fs');
 
 console.log('Server started.');
 
@@ -215,7 +218,7 @@ router.get('/TimeAndExpense', function(req, res) {
 	con.query(selectQuery, function(err, rows) {
 	  if (!err) {
 		console.log('The solution is: ', rows);
-		res.render('TimeAndExpense', {
+		res.render('ExpensePolicy', {
 			title: rows[0].name,
 			results: rows,
 			userlist: JSON.stringify(rows)
@@ -242,9 +245,21 @@ router.post('/upload', function(req, res) {
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
         console.log("Uploading: " + filename);
-        fstream = fs.createWriteStream((__dirname.split("\\routes"))[0] + '/uploads/' + filename);
+        fstream = fs.createWriteStream((__dirname.split("\\routes"))[0] + '/private/tmp/' + filename);
         file.pipe(fstream);
+		var key = 'EncryptSecretKey1423';
+		var input = path.resolve(__dirname+'/../private/tmp/' + filename);
+		var output = path.resolve(__dirname+'/../private/tmp/file.dat');// + filename);
+		var options = { algorithm : 'aes256' };
+		encryptor.encryptFile(input, output, key, options, function(err) {
+			//res.download(output);   
+		});
+		/* Delete file off of server */
+		//var filePath = __dirname+'/../tmp/private/' + filename; 
+		//fs.unlinkSync(filePath);
+		var filePath = __dirname+'/../private/tmp/' + filename; 
         fstream.on('close', function () {
+			fs.unlinkSync(filePath);
             res.redirect('back');
         });
     });
@@ -255,8 +270,24 @@ router.get('/uploadFileFunc', function(req, res) {
 	fs.readFile(req.files.datafile.path, function (err, data) { 
 		var newPath = __dirname + "/uploads/" + datafile.name;
 		fs.writeFile(newPath, data, function (err) {
-			res.redirect("back"); 
+			var key = 'EncryptSecretKey1423';
+			var input = path.resolve(__dirname+'/../uploads/' + datafile.name);
+			var output = path.resolve(__dirname+'/../private/tmp/file.dat');
+			var options = { algorithm : 'aes256' };
+			encryptor.encryptFile(input, output, key, options, function(err) {
+				//res.download(output);   
+			});
+			res.redirect("back");
 		}); 
+		var source = fs.createReadStream(__dirname + '/../private/tmp/file.dat');
+		var dest = fs.createWriteStream(__dirname + '/../private/+ datafile.name');
+		/* Put file in private folder*/
+		source.pipe(dest);
+		source.on('end', function() { /* copied */ });
+		source.on('error', function(err) { console.log('Move encrypted file error'); /* error */ });
+		/* Delete file off of server */
+		var filePath = __dirname+'/../uploads/' + datafile.name; 
+		fs.unlinkSync(filePath);
 	});
 });
 
@@ -304,5 +335,26 @@ router.post('/adduser', function(req, res) {
         }
     });
 });*/
+
+router.get("/encrypt",function(req,res){
+   var key = 'EncryptSecretKey1423';
+   var input = path.resolve(__dirname+'/../private/test.pdf');
+   var output = path.resolve(__dirname+'/../private/tmp/file.dat');
+   var options = { algorithm : 'aes256' };
+   encryptor.encryptFile(input, output, key, options, function(err) {
+        //res.download(output);   
+	});
+});
+
+router.get("/decrypt",function(req,res){
+       
+   var key = 'EncryptSecretKey1423';
+   var input = path.resolve(__dirname+'/../private/tmp/file.dat');
+   var output = path.resolve(__dirname+'/../private/test.pdf');
+   var options = { algorithm : 'aes256' };
+   encryptor.decryptFile(input, output, key, options, function(err) {
+        //res.download(output);  
+   });
+ });
 
 module.exports = router;
